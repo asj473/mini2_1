8181a6d4d631c4829d1ad70f8c18a32260ca8d91
asj473 <asj473@gmail.com>
2017-07-04 17:45:53 +0900

    기본 설정

---
+++ themat/src/main/java/kr/co/mlec/main/Main.java
@@ -0,0 +1,11 @@
+package kr.co.mlec.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Main {
+	@RequestMapping("/main/main.do")
+	public void main() throws Exception{
+	}
+}
---
+++ themat/src/main/java/kr/co/mlec/member/Member.java
@@ -0,0 +1,5 @@
+package kr.co.mlec.member;
+
+public class Member {
+
+}
