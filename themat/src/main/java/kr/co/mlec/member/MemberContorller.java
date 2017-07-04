658833cfdb9f01b84e5ce28079edacaede84799c
asj473 <asj473@gmail.com>
2017-07-04 21:38:43 +0900

    기본 설정중

--- themat/src/main/java/kr/co/mlec/member/MemberContorller.java
+++ themat/src/main/java/kr/co/mlec/member/MemberContorller.java
@@ -1,26 +1,46 @@
 package kr.co.mlec.member;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.co.mlec.vo.UserVO;
 
 @Controller
 @RequestMapping("/member")
 public class MemberContorller {
 
-	@RequestMapping("/loginForm.do")
-	public void loginform(){
-		
-		
-		
-	}
-	@RequestMapping("/joinForm.do")
-	public void joinForm(){
+
+	@RequestMapping("/login.do")
+	public void login(){
+		System.out.println("aaaa");
 		
 		
 		
 	}
 
+	@RequestMapping("/join.do")
+	public void join(){
+		System.out.println("1111");
+		
+		
+	}
+	
+	
+	@RequestMapping("/loginNext.do")
+	@ResponseBody
+	public String loginNext(HttpServletRequest request){
+		System.out.println("2222");
+		System.out.println("");
 
-
+		System.out.println(request.getParameter("id"));
+		String id = request.getParameter("id");
+		UserVO user = new UserVO();
+		user.setId(id);
+		
+		return "user";
+	}
 
 }
---
+++ themat/src/main/java/kr/co/mlec/vo/UserVO.java
@@ -0,0 +1,21 @@
+package kr.co.mlec.vo;
+
+public class UserVO {
+
+	private String id;
+	private String pass;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+}
